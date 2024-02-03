90419c54085bedd2a53c3909d713fc6c5550b9ad
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2024-02-03 00:16:15 -0800

    Refactored text input to own component, styled components

---
+++ website/src/components/LLMTextInput/LLMTextInput.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./llmtextinput.css";
+
+function LLMTextInput({ rating = "Good Good" }) {
+    const [llmText, setLlmText] = useState("");
+
+    const onTextSubmit = () => {
+        console.log(llmText);
+
+        fetch("http://127.0.0.1:5000/prompt", {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({"input": llmText}),
+        })
+        .then((res) => res.text())
+        .then((data) => console.log(data)); // The capital of Germany is Berlin.
+    }
+
+    return (
+        <div className="llm-text-container">
+            <div>Why did you choose {rating}?</div>
+            <textarea className="llm-text-input" placeholder="I chose this because..." type="text" onChange={(event) => setLlmText(event.target.value)}></textarea>
+            <button className="llm-text-submit" onClick={onTextSubmit}>Submit</button>
+        </div>
+    );
+}
+
+export default LLMTextInput;
--- website/src/pages/scenario1/Scenario1.js
+++ website/src/pages/scenario1/Scenario1.js
@@ -1,19 +1,19 @@
 import ThreeCube from "../../components/ThreeCube/ThreeCube";
 import { Button, Modal } from "react-bootstrap";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../scenarios.css";
 import RatingGrid from "../../components/RatingGrid/RatingGrid";
+import LLMTextInput from "../../components/LLMTextInput/LLMTextInput";
 
 function Scenario1() {
     const [show, setShow] = useState(false);
-    const [llmText, setLlmText] = useState("");
     const [rating, setRating] = useState("");
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
-    const onRating = (rating) => {
-        switch (rating) {
+    const onRating = (ratingScore) => {
+        switch (ratingScore) {
             case 0:
                 console.log("Good Good");
                 setRating("Good Good");
@@ -53,20 +53,6 @@ function Scenario1() {
         }
     }
 
-    const onTextSubmit = () => {
-        console.log(llmText);
-
-        fetch("http://127.0.0.1:5000/prompt", {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({"input": llmText}),
-        })
-        .then((res) => res.text())
-        .then((data) => console.log(data)); // The capital of Germany is Berlin.
-    }
-
     return (
         <div className="App">
             <div className="overlay">
@@ -74,10 +60,8 @@ function Scenario1() {
                     <RatingGrid onRating={onRating}/>
                 </div>
 
-                <div className="llm-text-container">
-                    <div>Why did you choose {rating}?</div>
-                    <textarea className="llm-text-input" type="text" onChange={(event) => setLlmText(event.target.value)}></textarea>
-                    <button className="llm-text-submit" onClick={onTextSubmit}>Submit</button>
+                <div className="llm-text">
+                    <LLMTextInput rating={rating}/>
                 </div>
             </div>
 
