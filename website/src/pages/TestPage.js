dac54582bc89fe8ddd654ddd933c22afba6375b2
jaczhi <54284925+jaczhi@users.noreply.github.com>
2024-02-02 21:22:28 -0800

    Website skeleton

---
+++ website/src/pages/TestPage.js
@@ -0,0 +1,36 @@
+import ThreeCube from "../components/ThreeCube";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
+
+function TestPage() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div className="App">
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <ThreeCube />
+    </div>
+  );
+}
+
+export default TestPage;
