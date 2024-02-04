c13422e5881bd00acd241fc8032ab5478124e5e8
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2024-02-03 19:14:40 -0800

    Added home page

--- website/src/App.js
+++ website/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { useNavigate } from "react-router-dom";
-import {Button} from 'react-bootstrap';
 
+import scenario1 from "./assets/img/scenario1.png";
+import scenario2 from "./assets/img/scenario2.png";
+import scenario3 from "./assets/img/scenario3.png";
+import scenario4 from "./assets/img/scenario4.png";
 
 function App() {
   const navigate = useNavigate();
 
-  return (
+  /*return (
     <>
       <Button onClick={() => {navigate("/scenario1")}}>Scenario 1</Button>
       <br />
@@ -17,6 +20,49 @@ function App() {
       <Button onClick={() => {navigate("/scenario4")}}>Scenario 4</Button>
       <br />
     </>
+  );*/
+
+  return (
+    <div className="container">
+      {/* Header */}
+      <div className="header">PRideal World</div>
+
+      {/* Grid Container */}
+      <div className="grid-container">
+        <div className="image-container" onClick={() => {navigate("/scenario1")}}>
+          <img
+            src={scenario1}
+            width={500}
+            height={250}
+            className="scenario-image"
+          />
+        </div>
+        <div className="image-container" onClick={() => {navigate("/scenario2")}}>
+          <img
+            src={scenario2}
+            width={500}
+            height={250}
+            className="scenario-image"
+          />
+        </div>
+        <div className="image-container" onClick={() => {navigate("/scenario3")}}>
+          <img
+            src={scenario3}
+            width={500}
+            height={250}
+            className="scenario-image"
+          />
+        </div>
+        <div className="image-container" onClick={() => {navigate("/scenario4")}}>
+          <img
+            src={scenario4}
+            width={500}
+            height={250}
+            className="scenario-image"
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
